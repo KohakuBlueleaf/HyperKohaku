60c3841956541b75af0cb7f109762ba1a3859609
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-07-24 17:59:01 +0800

    match the example scripts

--- train_hyperdreambooth.ps1
+++ train_hyperdreambooth.ps1
@@ -2,19 +2,23 @@
 python -m accelerate.commands.launch $launch_args `
   --num_processes=1 --num_cpu_threads_per_process=8 "./train_hyperdreambooth.py" `
   --pretrained_model_name_or_path "./models/kohaku-v2.1" `
-  --instance_data_dir "F:\nn\datasets\celeba-hq-512x512" `
+  --instance_data_dir "F:\nn\datasets\celeba-hq-512-4.8k" `
   --instance_prompt "A [V] face" `
   --output_dir "./outputs" `
   --resolution 512 `
-  --num_train_epochs 10 `
   --learning_rate 0.00002 `
   --lr_scheduler cosine `
   --lr_warmup_steps 100 `
-  --use_8bit_adam `
   --allow_tf32 `
   --enable_xformers_memory_efficient_attention `
-  --gradient_checkpointing `
   --pre_compute_text_embeddings `
   --checkpoints_total_limit 2 `
+  --checkpointing_steps 500 `
+  --rank 4 `
+  --down_dim 100 `
+  --up_dim 50 `
+  --train_batch_size 4 `
+  --mixed_precision "bf16" `
+  --pre_opt_weight_path "./models/pre-optimized"
 
 Write-Output "Train finished"
--- train_preoptimized_liloras.ps1
+++ train_preoptimized_liloras.ps1
@@ -2,17 +2,18 @@
 python -m accelerate.commands.launch $launch_args `
   --num_processes=1 --num_cpu_threads_per_process=8 "./train_preoptimized_liloras.py" `
   --pretrained_model_name_or_path "./models/kohaku-v2.1" `
-  --instance_data_dir "F:\nn\datasets\celeba-hq-512x512" `
+  --instance_data_dir "F:\nn\datasets\celeba-hq-512-4.8k" `
   --instance_prompt "A [V] face" `
   --output_dir "./outputs" `
   --resolution 512 `
-  --learning_rate 0.001 `
+  --learning_rate 0.01 `
   --lr_scheduler constant `
   --allow_tf32 `
   --enable_xformers_memory_efficient_attention `
   --pre_compute_text_embeddings `
   --checkpoints_total_limit 2 `
   --checkpointing_steps 500 `
+  --train_steps_per_identity 10 `
   --rank 1 `
   --down_dim 96 `
   --up_dim 48 `
